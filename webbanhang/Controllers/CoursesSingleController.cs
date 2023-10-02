fdea4d2f2096bb0a743368e171f529a4eb3f8155
leanhnhat2002 <leanhnhatcontact@gmail.com>
2023-10-02 20:04:24 +0700

    update

---
+++ webbanhang/Controllers/CoursesSingleController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace webbanhang.Controllers
+{
+    public class CoursesSingleController : Controller
+    {
+        // GET: CoursesSingle
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ webbanhang/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using webbanhang.Context;
+
+namespace webbanhang.Controllers
+{
+    public class HomeController : Controller
+    {
+        websiteBHEntities objwebsiteBHEntities = new websiteBHEntities();
+        public ActionResult Index()
+        {
+            var listProduct = objwebsiteBHEntities.Products.ToList();
+            return View();
+        }
+
+    }
+}
